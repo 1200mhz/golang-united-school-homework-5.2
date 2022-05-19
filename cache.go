41c16a2f7d9a3c8a2a5f630c23198a7b2b50c2ba
Eldar Azimov <chaevnicher@gmail.com>
2022-05-20 02:54:51 +0300

    HW-0052: code only

--- cache.go
+++ cache.go
@@ -1,23 +1,61 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+type Data struct {
+	Value    string
+	deadline *time.Time
+}
 
 type Cache struct {
+	mu   sync.Mutex
+	data map[string]Data
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
 
+	if val, ok := cache.data[key]; ok && cache.data[key].deadline.After(time.Now()) {
+		return val.Value, true
+	}
+
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = Data{value, nil}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	keys := make([]string, len(cache.data))
+	now := time.Now()
+
+	for k, v := range cache.data {
+		if v.deadline.After(now) {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = Data{value, &deadline}
 }
